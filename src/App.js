5b3154b99bc428fa4b8e8b79d846c0f3021f9376
Maxime Graindor <maxime.graindor@hotmail.com>
2021-01-10 01:00:12 +0100

    Create components and start route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-
+import {BrowserRouter, Redirect, Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
-  return (
-    <div className="App">
-     <p>
-       Hello world
-     </p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Redirect to="/home" />
+
+				<Switch>
+					<Route path="/home" >
+						<Home />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
