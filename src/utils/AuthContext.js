f3c02f081eda2d42f190432cc5dca73b4f5428a8
Maxime Graindor <maxime.graindor@hotmail.com>
2021-01-10 11:03:43 +0100

    Add context for current user

---
+++ src/utils/AuthContext.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState, createContext} from 'react'
+import firebase from './firebase'
+
+export const AuthContext = createContext();
+
+export const AuthProvider = (props) => {
+    
+    const [currentUser, setCurrentUser] = useState()
+
+    useEffect(() => {
+
+        firebase.auth().onAuthStateChanged(setCurrentUser);
+
+    }, [])
+    
+    return(
+        <AuthContext.Provider value={[currentUser, setCurrentUser]}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
