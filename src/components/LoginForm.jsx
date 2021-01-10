b71135c31b218f8e3eeeb1d164407fa665a61174
Maxime Graindor <maxime.graindor@hotmail.com>
2021-01-10 11:11:21 +0100

    Add login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, {useState, Fragment, useContext} from 'react'
+import firebase from '../utils/firebase'
+import {AuthContext} from '../utils/AuthContext' 
 
 export default function LoginForm() {
+
+    const [currentUser, setCurrentUser] = useContext(AuthContext)
+
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+
+    const authListener = (e) => {
+        e.preventDefault()
+
+        firebase.auth().signInWithEmailAndPassword(user, password)
+        .then((user) => {
+            setCurrentUser(user);
+          })
+          .catch((error) => {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+          });
+    }
+
     return (
         <div>
             <p>
-                Login Form
+                Formulaire de connection
             </p>
+            <form>
+                <p>
+                    <label htmlFor="email">Email</label>
+                    <input type="mail" name="email" id="email" placeholder="maxime.graindor@hotmail.com" onChange={e => setUser(e.target.value)} />
+                </p>
+                <p>
+                    <label htmlFor="password">Mot de passe</label>
+                    <input type="password" name="password" id="password" onChange={e => setPassword(e.target.value)}/>
+                </p>
+                <input type="submit" value="Se connecter" onClick={authListener}/>
+            </form>
+
+            {currentUser ? currentUser.email : <div> test </div>}
+
         </div>
     )
 }
