c67920905627573f15933c5eae2b65c15822d05d
Maxime Graindor <maxime.graindor@hotmail.com>
2021-01-10 15:40:48 +0100

    try to display birds

--- src/components/Encyclopedia.jsx
+++ src/components/Encyclopedia.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+import Firebase from '../utils/firebase'
 
 export default function Encyclopedia() {
+
+    const [encyclopedie, setEncyclopedie] = useState([])
+
+    useEffect(() =>{
+
+        const db = Firebase.firestore()
+
+        const tempData = []
+
+        db.collection('encyclopedie')
+        .get()
+        .then(snapshot =>{
+            snapshot.docs.forEach(doc =>{
+                setEncyclopedie(encyclopedie.push(doc.data()))
+            })
+        })
+        console.log(encyclopedie)
+
+    }, [])
+
     return (
         <div>
             <p>
                 ENCYCLOPEDIE
             </p>
+
+            <div>
+            </div>
         </div>
     )
 }
