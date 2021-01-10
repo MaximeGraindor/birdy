8da2c3db79ca16955e7058cc881e3017c243009d
Maxime Graindor <maxime.graindor@hotmail.com>
2021-01-10 10:30:16 +0100

    Fix the route between components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import {BrowserRouter, Redirect, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Redirect, Switch, Route, Link} from 'react-router-dom'
 import Home from './components/Home'
+import LoginForm from './components/LoginForm'
+import SignUpForm from './components/SignUpForm'
 
 function App() {
 	return (
@@ -7,9 +9,21 @@ function App() {
 			<BrowserRouter>
 				<Redirect to="/home" />
 
+				<div>
+					<Link to="/home">Home</Link>
+					<Link to="/login">Login</Link>
+					<Link to="/signup">Sign up</Link>
+				</div>
+
 				<Switch>
 					<Route path="/home" >
 						<Home />
+					</Route>
+					<Route path="/login" >
+						<LoginForm />
+					</Route>
+					<Route path="/signup" >
+						<SignUpForm />
 					</Route>
 				</Switch>
 			</BrowserRouter>
