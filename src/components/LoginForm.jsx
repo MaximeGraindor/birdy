2a592b0bcbed2f743e1f5907c3875f0988e0cdeb
Maxime Graindor <maxime.graindor@hotmail.com>
2021-01-16 18:50:02 +0100

    Fixed retrieving data from users

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -19,6 +19,7 @@ export default function LoginForm() {
           .catch((error) => {
             let errorCode = error.code;
             let errorMessage = error.message;
+            console.log(error.code + ": " + errorMessage);
           });
     }
 
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Firebase from '../utils/firebase'
 
 export default function Users() {
+
+    const [users, setUsers] = useState( [] )
+
+    useEffect(() => {
+        const db = Firebase.firestore()
+        db.collection('users').get()
+            .then(querySnapshot => {
+                const data = querySnapshot.docs.map(doc => doc.data())
+                setUsers(data);
+            })
+    }, [])
+
     return (
         <div>
             <p>
-                LISTE DES UTILISATEURS
+                USERS
             </p>
+            <div>
+                {
+                    users.map(user => <p>{user.name}</p>)
+                }
+            </div>
         </div>
     )
 }
