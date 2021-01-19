16dbb9634ed0093f1149f846575bb46789054972
Maxime Graindor <maxime.graindor@hotmail.com>
2021-01-20 00:51:06 +0100

    Display the captured list from the user

--- src/components/CaptureList.jsx
+++ src/components/CaptureList.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React,{useEffect, useState, useContext} from 'react'
+import Firebase from '../utils/firebase'
+import {AuthContext} from '../utils/AuthContext' 
 
 export default function CaptureList() {
+
+    const [currentUser, setCurrentUser] = useContext(AuthContext)
+    const [captures, setCaptures] = useState([])
+
+    useEffect(() => {
+        const db = Firebase.firestore()
+        db.collection('capture_list').where("user_id", "==", currentUser.uid)
+        .get()
+        .then(querySnapshot => {
+            const data = querySnapshot.docs.map(doc => doc.data())
+            setCaptures(data);
+        })
+    }, [])
+    
     return (
         <div>
             <p>
                 LISTE DE CAPTURE
             </p>
+            
+            <div>
+                {
+                    captures.map(capture => 
+                            <span>{capture.latin_name}</span>
+                    )
+                }
+            </div>
+
         </div>
     )
 }
